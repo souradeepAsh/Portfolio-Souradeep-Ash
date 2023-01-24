4097d2115d67303c4237cd9ca7302ef8395815d8
KingAsh <71181902+souradeepAsh@users.noreply.github.com>
2023-01-25 00:57:53 +0530

    change in css mainly and add mailing and other staff

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Portfolio_Model.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/kda.mp4",
+    },
+];
