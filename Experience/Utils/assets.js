14c345cbecc76e43c386c723c4619dbccf980744
KingAsh <71181902+souradeepAsh@users.noreply.github.com>
2023-05-16 15:13:13 +0530

    change done

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Portfolio_Model-v1.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/kda.mp4",
+    },
+];
---
+++ index.js
@@ -0,0 +1,31 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_mbpx6nj";
+    const templateID = "template_7im9h8d";
+  
+      emailjs
+      .send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  }
+
+  
+  function linkedinGo() {
+    window.open('', '_open');
+  }
+
+  function instaGo() {
+    window.open('', '_open');
+  }
